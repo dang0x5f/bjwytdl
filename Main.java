33270aa9d38ac2154b5b5340c75e77cc45fc4eb6
dang0x5f <dannypgehrz@gmail.com>
2024-12-22 16:59:45 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,15 +16,21 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 
 import javax.swing.JRootPane;
+import javax.swing.JTable;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JProgressBar;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JComboBox;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.WindowConstants;
@@ -96,13 +102,59 @@ public class Main
         pgb1.setMaximum(100);
         pgb1.setValue(25);
 
+        Object[][] data = new Object[2][3];
+        data[0][0] = "anoseuhnaou";
+        data[0][1] = "75";
+        data[0][2] = "1:24";
+        data[1][0] = "jbvwkjmbjvk";
+        data[1][1] = "39";
+        data[1][2] = "0:35";
+
+        String[] dl_colheadings = { "name", "progress", "time" };
+
+        JTable dl_table = new JTable(data,dl_colheadings);
+        TableColumnModel tcm = dl_table.getColumnModel();
+        TableColumn prog_col = tcm.getColumn(1);
+        prog_col.setCellRenderer(new ProgressCellRender());
+
+        JScrollPane dl_scrollpane = new JScrollPane(dl_table);
+
+        audio_downloads.add(dl_scrollpane);
+
 
         String audio_formats[] = { "aac", "alac", "flac", "m4a", "mp3", "opus", "vorbis", "wav" };
-        JList<String> list_box = new JList<String>(audio_formats);
+        // JList<String> list_box = new JList<String>(audio_formats);
+        JComboBox<String> list_box = new JComboBox<String>(audio_formats);
         // JScrollPane scroll_pane = new JScrollPane(list_box);
+        
+        JLabel format_label = new JLabel("format");
 
-        JPanel audio_options = new JPanel();
-        audio_options.add(list_box);
+        JButton dl_button = new JButton("download");
+
+        JTextField url_textfield = new JTextField(50);
+
+        JPanel audio_options = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc2 = new GridBagConstraints();
+        // gbc2.fill = GridBagConstraints.HORIZONTAL;
+        gbc2.fill = GridBagConstraints.BOTH;
+        gbc2.anchor = GridBagConstraints.PAGE_START;
+        gbc2.insets = new Insets(5,5,5,5);
+        gbc2.gridx = 0; 
+        gbc2.gridy = 0; 
+        audio_options.add(format_label,gbc2);
+        gbc2.gridx = 2; 
+        gbc2.gridy = 0; 
+        audio_options.add(list_box,gbc2);
+        gbc2.gridx = 0; 
+        gbc2.gridy = 1; 
+        gbc2.weightx = 1;
+        gbc2.gridwidth = 4;
+        audio_options.add(url_textfield,gbc2);
+        gbc2.gridx = 4; 
+        gbc2.gridy = 1; 
+        gbc2.weightx = 0;
+        gbc2.gridwidth = 1;
+        audio_options.add(dl_button,gbc2);
         // audio_panel.add(scroll_pane);
         // audio_panel.add(url_textfield);
 
@@ -146,4 +198,21 @@ public class Main
 
         frame.setVisible(true);
     }
+
+    public static class ProgressCellRender extends JProgressBar implements TableCellRenderer {
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            int progress = 0;
+            if (value instanceof Float) {
+                progress = Math.round(((Float) value) * 100f);
+            } else if (value instanceof Integer) {
+                progress = (int) value;
+            }
+            setValue(progress);
+            return this;
+        }
+    }
+
 }
+
